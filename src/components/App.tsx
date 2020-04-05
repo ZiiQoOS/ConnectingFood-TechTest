74d7e77f1e9e83bca4c7505b275e4af799cf5f37
ZiiQooS <ahmedzakariaesdairi@gmail.com>
2020-04-05 01:24:31 +0100

    Clean up the code base

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,4 @@
 import * as React from 'react'
-import { Toggle } from './Toggle'
+import './App.css'
 
-export const App = () => (
-  <Toggle onToggle={on => console.log('on: ', on)}>
-    <Toggle.On>The button is on</Toggle.On>
-    <Toggle.Off>The button is off</Toggle.Off>
-    <Toggle.Button />
-  </Toggle>
-)
+export const App = () => <div className={'App'}>Test</div>;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
-import { render } from 'react-dom'
+import {render} from 'react-dom'
 import './index.css'
+import {App} from './components/App'
 
-import { App } from './components/App'
-
-render(<App />, document.getElementById('root'))
+render(<App/>, document.getElementById('root'));
