15f2784a534e10cc703a79920fab7130d61b2031
ZiiQooS <ahmedzakariaesdairi@gmail.com>
2020-04-08 22:08:57 +0100

    Update App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,9 @@
 import * as React from 'react'
+import {CardsContainer} from './CardContainer/CardsContainer' ;
 import './App.css'
 
-export const App = () => <div className={'App'}>Test</div>;
+export const App = () =>
+    <div className={'App'}>
+        <div className={"header"}>Paris Weather Forecast</div>
+        <CardsContainer/>
+    </div>;
