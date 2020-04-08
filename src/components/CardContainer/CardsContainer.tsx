cabc10d574274f6eb3eec33a4b6468adb7266d9c
ZiiQooS <ahmedzakariaesdairi@gmail.com>
2020-04-09 00:47:56 +0100

    Apply Error handling enhacements in component

--- src/components/CardContainer/CardsContainer.tsx
+++ src/components/CardContainer/CardsContainer.tsx
@@ -5,22 +5,32 @@ import {WeatherCard} from '../WeatherCard/WeatherCard';
 import axios, {AxiosError, AxiosResponse} from 'axios';
 import Forecast from "../../Models/Forecast";
 import RequestHelper from "../../helpers/RequestHelper";
+import ErrorHandler from "../../helpers/ErrorHandler";
+import Error from "../../Models/Error";
 
 interface Props {
     city: string;
 }
 
 export const CardsContainer: React.FunctionComponent<Props> = ({city}) => {
-    
+
+
     const [list, setList] = useState([]);
+    const [error, setError] = useState<Error>({message: '', code: 0});
+
     useEffect(() => {
-        let requestConfig = RequestHelper.getRequestConfig('GET', 'data/2.5/forecast/daily', city);
+
+        let requestConfig = RequestHelper.getRequestConfig('data/2.5/forecast/daily', city);
         axios(requestConfig)
             .then((response: AxiosResponse) => setList(response.data.list))
-            .catch((error: AxiosError) => console.log(error.message));
+            .catch((error) => {
+                setError(error)
+            });
+
     }, []);
 
-
+    if (error.message)
+        return <div className="error">{ErrorHandler.handleError(error)}</div>;
     if (list.length <= 0)
         return <div className="blink">Waiting Server to respond ...</div>;
     else {
