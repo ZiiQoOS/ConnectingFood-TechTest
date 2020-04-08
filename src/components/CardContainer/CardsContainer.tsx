5156b931a4dc1ab17eabf4231a34c4e6db71c023
ZiiQooS <ahmedzakariaesdairi@gmail.com>
2020-04-08 22:03:26 +0100

    Create container for forecasting cards

---
+++ src/components/CardContainer/CardsContainer.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import {useEffect, useState} from "react";
+import './CardsContainer.css'
+import {WeatherCard} from '../WeatherCard/WeatherCard';
+import axios, {AxiosError, AxiosResponse} from 'axios';
+import Forecast from "../../Models/Forecast";
+import RequestHelper from "../../helpers/RequestHelper";
+
+export const CardsContainer: React.FunctionComponent = ({}) => {
+
+    const [list, setList] = useState([]);
+    useEffect(() => {
+        let requestConfig = RequestHelper.getRequestConfig('GET', 'data/2.5/forecast/daily', 'Paris');
+        axios(requestConfig)
+            .then((response: AxiosResponse) => setList(response.data.list))
+            .catch((error: AxiosError) => console.log(error.message));
+    }, []);
+
+
+    if (list.length <= 0)
+        return <div className="blink">Waiting Server to respond ...</div>;
+    else {
+        return (
+            <main className="container">
+                <div className="card-wrapper">
+                    {
+                        list.map((fc: Forecast) => <WeatherCard key={fc.dt} forecast={fc}/>)
+                    }
+                </div>
+            </main>
+        )
+    }
+};
